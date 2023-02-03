f86f4748e828af8247b209c72fa76cd2e55b1d79
Vasago <Vasago@DESKTOP-RCLQ48N>
2023-02-03 16:55:22 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/AreaLibrary.cs
@@ -0,0 +1,54 @@
+﻿namespace ClassLibrary
+{
+
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        private double _sideA;
+        private double _sideB;
+        private double _sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+
+        public override double GetArea()
+        {
+            double halfP = (_sideA + _sideB + _sideC) / 2.0;
+            //since we do not check for existing of an actual triangle this is going to throw an exception for results where halfP is going to be less than a side length
+            //for example result for input sides 3, 5, 15 is going to be an exception that we do not handle since such triangle cannot exist
+            return Math.Sqrt(halfP * (halfP - _sideA) * (halfP - _sideB) * (halfP - _sideC));
+        }
+
+        public Boolean isRight()
+        {
+            double squareA = _sideA * _sideA;
+            double squareB = _sideB * _sideB;
+            double squareC = _sideC * _sideC;
+            return squareA + squareB == squareC || squareA + squareC == squareB || squareB + squareC == squareA;
+        }
+    }
+
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+
+Shape circle = new Circle(12.4);
+//double a = 3.4;
+//double b = 15;
+//double c = 5;
+//Shape triangle = new Triangle(a, b, c);
+
+//Shape triangle = new Triangle(2, 100, 99);
+Triangle triangle = new Triangle(3, 4, 5);
+
+//double p = (a + b + c) / 2;
+//double squqareTimesTwo = p * Math.Abs(p - a) * Math.Abs(p - b) * Math.Abs(p - c);
+
+Console.WriteLine(circle.GetArea().ToString("#.#####"));
+//Console.WriteLine(p);
+//Console.WriteLine(squqareTimesTwo);
+Console.WriteLine(triangle.GetArea().ToString("#.#####"));
+Console.WriteLine(triangle.isRight().ToString());
